884ef43f8547b5692e0a777baec5e145a8c8b4c1
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-23 13:08:53 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,3 +1,4 @@
+
 //
 //  main.swift
 //  Experimentation5
@@ -38,27 +39,27 @@ let percent_2050: Double = max(0.01, min(dirty_2050 / dirty_2020, 0.99))
 let year_end: Double = 2100
 
 let delta_time: Double = year_end - 2020
-let yearly_rate = pow(M_E, log(percent_2050) / 30)
+let yearly_rate = exp(log(percent_2050) / 30)
 let log_rate = log(yearly_rate)
 
 func integral_body_1(x: Double) -> Double {
-  (1 / log_rate) * pow(M_E, x * log_rate)
+  (1 / log_rate) * exp(x * log_rate)
 }
 
 // formula acquired from Wolfram Alpha
 func integral_body_2(x: Double) -> Double {
   // pow(rate, x)
-  let numerator_pt_1 = pow(M_E, x * log_rate)
-  
+  let numerator_pt_1 = exp(x * log_rate)
+
   // for energy increasing 50% by 2050
   // energy = 1 + 0.5 * (x / 30)
   // energy = 1 + x / 60
   // energy = 1 + x / coeff
   let coeff: Double = 60
-  
+
   let numerator_pt_2 = (x + coeff) * log_rate - 1
   let denominator = coeff * (log_rate * log_rate)
-  
+
   return (numerator_pt_1 * numerator_pt_2) / denominator
 }
 
